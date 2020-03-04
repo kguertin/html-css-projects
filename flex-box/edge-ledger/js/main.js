c7b8097088c9540f43d26ec828bc641c3f6dd21e
Kevin <kevin.m.guertin@gmail.com>
2020-03-03 20:25:49 -0500

    add jquery smooth scroll animation and nav opacity

--- flex-box/edge-ledger/js/main.js
+++ flex-box/edge-ledger/js/main.js
@@ -11,3 +11,28 @@ function initMap() {
   // The Marker Positioned at Location
   const marker = new google.maps.Marker({postion: loc, map: map});
 }
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
+
+// Stricky Menu Background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
